92a43313760a4d03f5e35aa7afdc639a1f41c625
agung <agungjati94@gmail.com>
2022-12-06 11:11:10 +0700

    add try catch

--- getReport.js
+++ getReport.js
@@ -12,13 +12,20 @@ const fields = ['Appintment ID', 'Booking Code', 'Time Task 1', 'Time Task 2', '
         values[i] = {}
         values[i]['Appintment ID'] = appt.appointment_id;
         values[i]['Booking Code'] = appt.booking_code;
-        const resp = await getTaskListApi(appt.booking_code);
-        const list = resp.response && resp.response.list || [];
-        list.forEach(ls => {
-            values[i]['Time Task '+ ls.taskid] = ls.waktu;
-        });
+        console.log('[start]', appt.booking_code)
+        try {
+            const resp = await getTaskListApi(appt.booking_code);
+            const list = resp.response && resp.response.list || [];
+            list.forEach(ls => {
+                values[i]['Time Task '+ ls.taskid] = ls.waktu;
+            });
+        }catch(er){
+            console.log('[error]', appt.booking_code, er.message)
+        }
+        
     }));
     
+    console.log('[start] create report')
     await createaReport(values);
 
 })();
